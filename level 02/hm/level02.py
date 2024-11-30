d1c8c2096e4e5014c72ca19e7a7f077c94d50c25
kasandroak <davit.kakutashvili@yahoo.com>
2024-11-30 20:23:09 +0400

    first commit

---
+++ level 02/hm/level02.py
@@ -0,0 +1,16 @@
+familly
+
+sandro kakutashvili
+mariam kakutashvili
+nuca kakutashvili
+maka bidzinashvili
+dato kakutashvili
+
+friends
+andria beridze
+andria barliani
+andria tavarkiladze
+andria zamtaradze
+vaniko jamburia
+andrea katsitadze
+.................
